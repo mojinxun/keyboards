4b2e44e83823ec0ce0810e303700d5e8f38224b9
姚烽 <yaofeng@fengwall.com>
2024-02-23 18:30:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+function getNowTimeName () {
+  const time = new Date()
+  const hour = time.getHours()
+  return hour < 9 ? '早上好' : hour <= 11 ? '上午好' : hour <= 13 ? '中午好' : hour < 20 ? '下午好' : '晚上好'
+}
+
+module.exports = getNowTimeName
